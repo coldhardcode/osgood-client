90ecceae29c4a1674c997481fd62fcb2f0cb79d7
Cory Watson <gphat@Crankwizzah.local>
2010-03-15 13:05:28 -0500

    Version bump (right this time).

--- lib/Osgood/Client.pm
+++ lib/Osgood/Client.pm
@@ -24,7 +24,7 @@ has 'url' => (
 has 'list' => ( is => 'rw', isa => 'Maybe[Osgood::EventList]' );
 has 'timeout' => ( is => 'rw', isa => 'Int', predicate => 'has_timeout' );
 
-our $VERSION = '2.0.6';
+our $VERSION = '2.0.7';
 our $AUTHORITY = 'cpan:GPHAT';
 
 =head1 NAME
